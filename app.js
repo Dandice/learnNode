fbadda411fca268c567ba757fe38ecc4fc399b5c
Dandice <544483418@qq.com>
2016-05-16 15:44:19 +0800

    start use mongoose

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express')
 var path = require('path')
+var mongoose = require('mongoose')
 var bodyParser = require('body-parser');
+var _=require('underscore')
+var Movie = require('./models/movie')
 var port = process.env.PORT || 3000
 var app = express()
+
+mongoose.connect('mongodb://localhost/learnNode' + port)
 
 app.set('views', './views/pages')
 app.set('view engine', 'jade')
@@ -17,40 +22,21 @@ console.log('iMovie started on port ' + port)
 
 //index page
 app.get('/', function (req, res)  {
-	res.render('index', {
-		title:'iMovie 首页',
-		movies: [{
-			title:'奇妙世纪 08 梦的还原器',
-			_id: 1,
-			poster:'http://r3.ykimg.com/05410408548589706A0A4160AF2742DF'
-		},{
-			title:'奇妙世纪 08 梦的还原器',
-			_id: 2,
-			poster:'http://r3.ykimg.com/05410408548589706A0A4160AF2742DF'
-		},{
-			title:'奇妙世纪 08 梦的还原器',
-			_id: 3,
-			poster:'http://r3.ykimg.com/05410408548589706A0A4160AF2742DF'
-		},{
-			title:'奇妙世纪 08 梦的还原器',
-			_id: 4,
-			poster:'http://r3.ykimg.com/05410408548589706A0A4160AF2742DF'
-		},{
-			title:'奇妙世纪 08 梦的还原器',
-			_id: 5,
-			poster:'http://r3.ykimg.com/05410408548589706A0A4160AF2742DF'
-		},{
-			title:'奇妙世纪 08 梦的还原器',
-			_id: 6,
-			poster:'http://r3.ykimg.com/05410408548589706A0A4160AF2742DF'
-		}]
+	Movie.fetch(function (err, movies) {
+		if (err) {
+			console.log(err)
+		}
+		res.render('index',{
+			title: 'home',
+			movies: movies
+		})
 	})
 })
 
 //admin page
 app.get('/admin/movie', function (req, res)  {
 	res.render('admin', {
-		title:'iMovie 后台管理',
+		title:'iMovie 后台录入页',
 		movie: {
 			title: ' ',
 			doctor: ' ',
@@ -66,35 +52,79 @@ app.get('/admin/movie', function (req, res)  {
 
 //detail page
 app.get('/movie/:id', function (req, res)  {
-	res.render('detail', {
-		title: 'iMovie 影片详情页',
-		movie: {
-			title: '奇妙世纪 08 梦的还原器',
-			doctor: '程亮/林博',
-			country: '大陆',
-			year: 2014,
-			language: '汉语',
-			poster: 'http://r3.ykimg.com/05410408548589706A0A4160AF2742DF',
-			flash: 'http://player.youku.com/player.php/sid/XODQ0NDk4MTA0/v.swf',
-			summary: '《奇妙世纪》是由啼声影视与优酷出品共同打造的中国首部原创都市奇幻单元剧。'
-              }
+	var id =  req.params.id
+	Movie.findById(id, function (err, movie) {
+		res.render('detail', {
+			title:'iMovie detail',
+			movie: movie
+		})		
 	})
+})
+
+// admin update movie
+app.get('/admin/update/:id', function (req, res) {
+	var id = req.params.id
+
+	if (id) {
+		Movie.findById(id, function (err, movie) {
+			res.render('admin', {
+				title: 'imoooc 后台更新页',
+				movie: movie
+			})
+		})
+	}
+})
+
+// admin post movie
+app.post('/admin/movie/new', function (req, res) {
+	var id = req.body.movie._id
+	var movieObj = req.body.movie
+	var _movie
+	// 已经存过的
+	if (id !== 'undefined') {
+		Movie.findById(id, function (err, movie) {
+			if (err) {
+				console.log(err)
+			}
+
+			_movie = _.extend(movie, movieObj)
+			_movie.save(function (err, movie) {
+				if (err){
+					console.log(err)
+				}
+				res.redirect('/movie/' + movie._id)
+			})
+		})
+	} else {
+		_movie = new Movie({
+			doctor: movieObj.doctor,
+			title: movieObj.title,
+			country: movieObj.country,
+			language: movieObj.language,
+			year: movieObj.year,
+			poster: movieObj.poster,
+			summary: movieObj.summary,
+			flash: movieObj.flash
+		})
+		_movie.save(function (err,movie) {
+			if (err){
+				console.log(err)
+			}
+			res.redirect('/movie/' + movie._id)
+		})
+	}
+
 })
 
 //list page
 app.get('/admin/list', function (req, res)  {
-	res.render('list', {
-		title:'iMovie 后台-影片列表',
-		movies: [{
-			_id: 1,
-			title: '奇妙世纪 08 梦的还原器',
-			doctor: '程亮/林博',
-			country: '大陆',
-			year: 2014,
-			language: '汉语',
-			summary: '《奇妙世纪》是由啼声影视与优酷出品共同打造的中国首部原创都市奇幻单元剧。',
-			poster: 'http://r3.ykimg.com/05410408548589706A0A4160AF2742DF',
-			flash: 'http://player.youku.com/player.php/sid/XODQ0NDk4MTA0/v.swf'
-		}]
+	Movie.fetch(function (err, movies) {
+		if (err) {
+			console.log(err)
+		}
+		res.render('index',{
+			title: 'list 列表页',
+			movies: movies
+		})
 	})
 })
